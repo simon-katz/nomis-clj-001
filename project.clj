a210f363e182f92b5a42c69cb20ed5fbe7294bb1
Simon Katz <sk@nomistech.com>
2016-06-19 15:05:00 +0100

    Revert "Change version to "0.1.0-SNAPSHOT""
    This reverts commit dcf81442e6f4d3d473b3fed2eefe3454df1b3ca7.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomis-clj-template-001/lein-template "0.1.0-SNAPSHOT"
+(defproject nomis-clj-template-001/lein-template "0.0.1"
   :description "Nomistech's first Clojure template"
   :url "https://github.com/simon-katz/nomis-clj-template-001"
   :license {:name "Eclipse Public License"
