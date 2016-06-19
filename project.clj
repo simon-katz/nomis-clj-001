440568c5bef16b12f9382d433b09d0a319ff37ad
Simon Katz <sk@nomistech.com>
2016-06-19 14:41:10 +0100

    Add description and URL to "project.clj"

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nomis-clj-template-001/lein-template "0.0.1"
-  :description "Created with lein-create-template"
-  :url "http://example.com/FIXME"
+  :description "Nomistech's first Clojure template"
+  :url "https://github.com/simon-katz/nomis-clj-template-001"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
