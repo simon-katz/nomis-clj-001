a54880510c63f8bf0b6ac6c9bef4d864b13e7f2b
Simon Katz <sk@nomistech.com>
2016-06-19 13:29:18 +0100

    Change all "notondef" to "instance-name"/"instance_name"

--- src/leiningen/new/x_template.clj
+++ src/leiningen/new/x_template.clj
@@ -19,20 +19,20 @@
                ["_scripts/run-tests"                                               (render' "_scripts/run-tests")]
                ["dev/user.clj"                                                     (render' "dev/user.clj")]
                
-               ["src/clj/{{sanitized}}/system/main.clj"                            (render' "src/clj/notondef/system/main.clj")]
-               ["src/clj/{{sanitized}}/system/system.clj"                          (render' "src/clj/notondef/system/system.clj")]
-               ["src/clj/{{sanitized}}/services/provided/web/server.clj"           (render' "src/clj/notondef/services/provided/web/server.clj")]
-               ["src/clj/{{sanitized}}/services/provided/web/handlers/handler.clj" (render' "src/clj/notondef/services/provided/web/handlers/handler.clj")]
+               ["src/clj/{{sanitized}}/system/main.clj"                            (render' "src/clj/instance_name/system/main.clj")]
+               ["src/clj/{{sanitized}}/system/system.clj"                          (render' "src/clj/instance_name/system/system.clj")]
+               ["src/clj/{{sanitized}}/services/provided/web/server.clj"           (render' "src/clj/instance_name/services/provided/web/server.clj")]
+               ["src/clj/{{sanitized}}/services/provided/web/handlers/handler.clj" (render' "src/clj/instance_name/services/provided/web/handlers/handler.clj")]
                
-               ["src/cljs/{{sanitized}}/main.cljs"                                 (render' "src/cljs/notondef/main.cljs")]
-               ["src/cljs/{{sanitized}}/render.cljs"                               (render' "src/cljs/notondef/render.cljs")]
-               ["src/cljs/{{sanitized}}/play/re_com_play.cljs"                     (render' "src/cljs/notondef/play/re_com_play.cljs")]
+               ["src/cljs/{{sanitized}}/main.cljs"                                 (render' "src/cljs/instance_name/main.cljs")]
+               ["src/cljs/{{sanitized}}/render.cljs"                               (render' "src/cljs/instance_name/render.cljs")]
+               ["src/cljs/{{sanitized}}/play/re_com_play.cljs"                     (render' "src/cljs/instance_name/play/re_com_play.cljs")]
                
                ["resources/public/assets/css/material-design-iconic-font.min.css"  (render' "resources/public/assets/css/material-design-iconic-font.min.css")]
                ["resources/public/assets/css/re-com.css"                           (render' "resources/public/assets/css/re-com.css")]
                ["resources/public/assets/css/bootstrap.css"                        (render' "resources/public/assets/css/bootstrap.css")]
                ["resources/public/css/style.css"                                   (render' "resources/public/css/style.css")]
-               ["resources/public/{{project-name}}.html"                           (render' "resources/public/notondef.html")]
+               ["resources/public/{{project-name}}.html"                           (render' "resources/public/instance-name.html")]
                
                ["resources/public/assets/css/chosen-sprite.png"                    (raw "resources/public/assets/css/chosen-sprite.png")]
                ["resources/public/assets/css/chosen-sprite@2x.png"                 (raw "resources/public/assets/css/chosen-sprite@2x.png")]
