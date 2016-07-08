9c940956063588c4f810ec15e2da12c5ed62c8f7
Simon Katz <sk@nomistech.com>
2016-07-08 16:09:37 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomis-clj-template-001/lein-template "0.0.3"
+(defproject nomis-clj-template-001/lein-template "0.0.4"
   :description "Nomistech's first Clojure template"
   :url "https://github.com/simon-katz/nomis-clj-template-001"
   :license {:name "Eclipse Public License"
